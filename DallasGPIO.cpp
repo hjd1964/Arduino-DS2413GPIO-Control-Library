df8350ec87f7144541b3e051112c29bd5cc30d71
Howard Dutton <hjd1964@gmail.com>
2020-03-05 12:05:18 -0500

    Initial commit
    This project is based on the Arduino-Temperature-Control-Library

---
+++ DallasGPIO.cpp
@@ -0,0 +1,216 @@
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+#include "DallasGPIO.h"
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+extern "C" {
+#include "WConstants.h"
+}
+#endif
+
+DallasGPIO::DallasGPIO()
+{
+    useExternalPullup = false;
+}
+DallasGPIO::DallasGPIO(OneWire* _oneWire)
+{
+    setOneWire(_oneWire);
+    useExternalPullup = false;
+}
+
+/*
+ * Constructs DallasGPIO with strong pull-up turned on. Strong pull-up is mandated in DS2413 datasheet for parasitic
+ * power (2 wires) setup. (https://datasheets.maximintegrated.com/en/ds/DS2413.pdf, p. 2, section 'ELECTRICAL CHARACTERISTICS').
+ */
+DallasGPIO::DallasGPIO(OneWire* _oneWire, uint8_t _pullupPin) : DallasGPIO(_oneWire){
+    setPullupPin(_pullupPin);
+}
+
+void DallasGPIO::setPullupPin(uint8_t _pullupPin) {
+    useExternalPullup = true;
+    pullupPin = _pullupPin;
+    pinMode(pullupPin, OUTPUT);
+    deactivateExternalPullup();
+}
+
+void DallasGPIO::setOneWire(OneWire* _oneWire) {
+
+    _wire = _oneWire;
+    devices = 0;
+    ds2413Count = 0;
+    parasite = false;
+    waitForConversion = true;
+    checkForConversion = true;
+}
+
+// initialise the bus
+void DallasGPIO::begin(void) {
+
+    DeviceAddress deviceAddress;
+
+    _wire->reset_search();
+    devices = 0;     // Reset the number of devices when we enumerate wire devices
+    ds2413Count = 0; // Reset number of DS2413 device
+
+    while (_wire->search(deviceAddress)) {
+
+        if (validAddress(deviceAddress)) {
+
+            if (!parasite && readPowerSupply(deviceAddress))
+                parasite = true;
+
+            bitResolution = max(bitResolution, getResolution(deviceAddress));
+
+            devices++;
+            if (validFamily(deviceAddress)) {
+                ds2413Count++;
+            }
+        }
+    }
+}
+
+// returns the number of devices found on the bus
+uint8_t DallasGPIO::getDeviceCount(void) {
+    return devices;
+}
+
+uint8_t DallasGPIO::getDS2413Count(void) {
+    return ds2413Count;
+}
+
+// returns true if address is valid
+bool DallasGPIO::validAddress(const uint8_t* deviceAddress) {
+    return (_wire->crc8(deviceAddress, 7) == deviceAddress[7]);
+}
+
+bool DallasTemperature::validFamily(const uint8_t* deviceAddress) {
+    switch (deviceAddress[0]) {
+    case DS2413MODEL:
+        return true;
+    default:
+        return false;
+    }
+}
+
+// finds an address at a given index on the bus
+// returns true if the device was found
+bool DallasGPIO::getAddress(uint8_t* deviceAddress, uint8_t index) {
+
+    uint8_t depth = 0;
+
+    _wire->reset_search();
+
+    while (depth <= index && _wire->search(deviceAddress)) {
+        if (depth == index && validAddress(deviceAddress))
+            return true;
+        depth++;
+    }
+
+    return false;
+}
+
+// set GPIO0 and GPIO1 HIGH or LOW by index
+bool DallasGPIO::setStateByIndex(uint8_t deviceIndex, uint8_t gpio0, uint8_t gpio1) {
+
+    DeviceAddress deviceAddress;
+    getAddress(deviceAddress, deviceIndex);
+
+    return setStateByAddress(deviceAddress, gpio0, gpio1);
+}
+
+// set GPIO0 and GPIO1 HIGH or LOW by address
+bool DallasGPIO::setStateByAddress(const uint8_t* deviceAddress, uint8_t gpio0, uint8_t gpio1, bool polling) {
+    static uint8_t stage = 0;
+    static uint8_t result = 0;
+
+    uint8_t state = 0xFF;
+    if (gpio0) state &= !0x01;
+    if (gpio1) state &= !0x02;
+
+    if (stage == 0) { _wire->reset(); lastError = false; stage++; if (polling) return; }
+    if (stage == 1) { if (_wire->select(address,true)) stage++; if (polling) return; }
+    if (stage == 2) { write(DS2413_ACCESS_WRITE); stage++; if (polling) return; }
+    if (stage == 3) { write(state); stage++; if (polling) return; }
+    if (stage == 4) { write(~state); stage++; if (polling) return; }          // invert data and send again    
+    if (stage == 5) { result = _wire->read(); stage++; if (polling) return; } // 0xAA=success, 0xFF=failure
+    if (stage == 6) {
+		if (result == DS2413_ACK_SUCCESS) _wire->read(); else lastError=true; // read the status byte
+		stage++;
+		if (polling) return;
+    }
+    if (stage == 7) { _wire->reset(); stage++; if (polling) return; }
+
+    if (stage == 8) stage = 0;
+
+    return stage == 0;
+}
+
+
+// get GPIO0 and GPIO1 HIGH or LOW by index
+bool DallasGPIO::getStateByIndex(uint8_t deviceIndex, uint8_t* gpio0, uint8_t* gpio1) {
+
+    DeviceAddress deviceAddress;
+    getAddress(deviceAddress, deviceIndex);
+
+    return getStateByAddress(deviceAddress, gpio0, gpio1);
+}
+
+// get GPIO0 and GPIO1 HIGH or LOW by address
+bool DallasGPIO::getStateByAddress(const uint8_t* deviceAddress, uint8_t* gpio0, uint8_t* gpio1, bool polling) {
+    static uint8_t stage = 0;
+    static uint8_t result = 0;
+
+    switch (stage) {
+	if (stage == 0) { _wire->reset(); lastError=false; stage++; if (polling) return; }
+	if (stage == 1) { if (_wire->select(address,polling)) stage++; if (polling) return; }
+	if (stage == 2) { _wire->write(DS2413_ACCESS_READ); stage++; if (polling) return; }
+	if (stage == 3) { result = oneWire.read(); stage++; if (polling) return; }
+	if (stage == 4) { _wire->reset(); stage++; if (polling) return; }
+	if (stage == 5) {
+		// check inverted nibble
+		if ((!result & 0x0F) == (result >> 4)) {
+		  if (result & 1) *gpio0=LOW; else *gpio0=HIGH;
+		  if (result & 2) *gpio1=LOW; else *gpio1=HIGH;
+		} else lastError=true;
+		stage++;
+		if (polling) return;
+	}
+
+    if (stage == 6) stage = 0;
+
+    return stage == 0;
+}
+
+// attempt to determine if the device at the given address is connected to the bus
+bool DallasGPIO::isConnected(const uint8_t* deviceAddress) {
+    uint8_t g0, g1;
+
+    return getStateByAddress(deviceAddress, &g0, &g1);
+}
+
+// returns true if parasite mode is used (2 wire)
+// always true for DS2413
+bool DallasGPIO::readPowerSupply(const uint8_t* deviceAddress)
+{
+    return true;
+}
+
+void DallasGPIO::activateExternalPullup() {
+    if(useExternalPullup)
+        digitalWrite(pullupPin, LOW);
+}
+
+void DallasGPIO::deactivateExternalPullup() {
+    if(useExternalPullup)
+        digitalWrite(pullupPin, HIGH);
+}
+
+// returns true if the bus requires parasite power
+bool DallasGPIO::isParasitePowerMode(void) {
+    return parasite;
+}
---
+++ DallasGPIO.h
@@ -0,0 +1,123 @@
+#ifndef DallasGPIO_h
+#define DallasGPIO_h
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+#include <inttypes.h>
+#include <OneWire.h>
+
+// Model IDs
+#define DS2413MODEL         0x3A
+
+// Command Codes
+#define DS2413_ACCESS_READ  0xF5
+#define DS2413_ACCESS_WRITE 0x5A
+#define DS2413_ACK_SUCCESS  0xAA
+
+// Error Codes
+#define DS2413_ACK_ERROR    0xFF
+
+// For readPowerSupply on oneWire bus
+#ifndef nullptr
+#define nullptr NULL
+#endif
+
+typedef uint8_t DeviceAddress[8];
+
+class DallasGPIO {
+public:
+
+	DallasGPIO();
+	DallasGPIO(OneWire*);
+	DallasGPIO(OneWire*, uint8_t);
+
+	void setOneWire(OneWire*);
+
+    void setPullupPin(uint8_t);
+
+	// initialise bus
+	void begin(void);
+
+	// returns the number of devices found on the bus
+	uint8_t getDeviceCount(void);
+
+	// returns the number of DS2413 Family devices on bus
+	uint8_t getDS2413Count(void);
+
+	// returns true if address is valid
+	bool validAddress(const uint8_t*);
+
+	// returns true if address is of the family of sensors the lib supports.
+	bool validFamily(const uint8_t* deviceAddress);
+
+	// finds an address at a given index on the bus
+	bool getAddress(uint8_t*, uint8_t);
+
+	// set GPIO0 and GPIO1 HIGH or LOW by index
+	bool setStateByIndex(uint8_t deviceIndex, uint8_t gpio0, uint8_t gpio1);
+
+	// set GPIO0 and GPIO1 HIGH or LOW by address
+	bool setStateByAddress(const uint8_t* deviceAddress, uint8_t gpio0, uint8_t gpio1, bool polling = false);
+
+	// get GPIO0 and GPIO1 HIGH or LOW by index
+    bool getStateByIndex(uint8_t deviceIndex, uint8_t* gpio0, uint8_t* gpio1);
+
+    // get GPIO0 and GPIO1 HIGH or LOW by address
+    bool getStateByAddress(const uint8_t* deviceAddress, uint8_t* gpio0, uint8_t* gpio1, bool polling = false);
+
+	// attempt to determine if the device at the given address is connected to the bus
+	bool isConnected(const uint8_t*);
+
+	// attempt to determine if the device at the given address is connected to the bus
+	bool isConnected(const uint8_t*, uint8_t*);
+
+	// read device's power requirements
+	bool readPowerSupply(const uint8_t* deviceAddress = nullptr);
+
+    // set GPIO0 and GPIO1 HIGH or LOW by index
+    bool setStateByIndex(uint8_t deviceIndex, uint8_t gpio0, uint8_t gpio1);
+    
+    // set GPIO0 and GPIO1 HIGH or LOW by address
+	bool setStateByAddress(const uint8_t* deviceAddress, uint8_t gpio0, uint8_t gpio1);
+
+    // get GPIO0 and GPIO1 state by index
+    bool getStateByIndex(uint8_t deviceIndex, uint8_t* gpio0, uint8_t* gpio1);
+    
+    // get GPIO0 and GPIO1 state by address
+	bool getStateByAddress(const uint8_t* deviceAddress, uint8_t* gpio0, uint8_t* gpio1);
+
+	// returns true if the bus requires parasite power
+	bool isParasitePowerMode(void);
+
+	// Is a conversion complete on the wire? Only applies to the first sensor on the wire.
+	bool isConversionComplete(void);
+
+private:
+    // error after last polling transaction
+	bool lastError = false;
+	
+	// parasite power on or off
+	bool parasite;
+
+	// external pullup
+	bool useExternalPullup;
+	uint8_t pullupPin;
+
+	// count of devices on the bus
+	uint8_t devices;
+
+	// count of DS2413 devices on bus
+	uint8_t ds2413Count;
+
+	// Take a pointer to one wire instance
+	OneWire* _wire;
+
+    // External pullup control
+    void activateExternalPullup(void);
+    void deactivateExternalPullup(void);
+
+};
+#endif
